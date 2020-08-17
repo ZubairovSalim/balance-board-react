dfdeb60e8057c4a28813d3ee13188b5a4b113516
salim <zubairov.salim@yandex.ru>
2020-08-17 18:01:38 +0500

    added Register page with Redux managing state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import SigninScreen from "./screens/SigninScreen";
 import {useSelector} from "react-redux";
+import RegisterScreen from "./screens/RegisterScreen";
 
 function App() {
     const userSignin = useSelector(state => state.userSignin);
@@ -45,6 +46,7 @@ function App() {
                 </aside>
                 <main className="main">
                     <div className="content">
+                        <Route path="/register" component={RegisterScreen} />
                         <Route path="/signin" component={SigninScreen} />
                         <Route path="/products/:id" component={ProductScreen} />
                         <Route path="/cart/:id?" component={CartScreen} />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from "redux-thunk";
 import Cookie from "js-cookie";
 import {productDetailsReducer, productListReducer} from "./reducers/productReducers";
 import {cartReducer} from "./reducers/cartReducers";
-import {userSigninReducer} from "./reducers/userReducers";
+import {userRegisterReducer, userSigninReducer} from "./reducers/userReducers";
 
 const cartItems = Cookie.getJSON("cartItems") || [];
 const userInfo = Cookie.getJSON("userInfo") || null;
@@ -15,6 +15,7 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userSignin: userSigninReducer,
+    userRegister: userRegisterReducer
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;

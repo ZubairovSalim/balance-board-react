c2e676ef0da6253e86f157508c902e4ad9a9b82e
salim <zubairov.salim@yandex.ru>
2020-08-17 15:33:42 +0500

    added userModel with MongoDB and sign in with JWT auth on backend

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export default {
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/balance_board_react',
+    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,27 @@
+const path = require('path')
 import express from 'express';
 import data from "./data";
+import dotenv from 'dotenv';
+import config from "./config";
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const  {PWD = __dirname} = process.env;
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use("/api/users", userRoute);
 
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
@@ -13,6 +33,14 @@ app.get("/api/products/:id", (req, res) => {
 
 app.get("/api/products", (req, res) => {
     res.send(data.products);
+});
+
+app.use('*', (req, res) => {
+    console.log(path.resolve(PWD, 'frontend'));
+    res.sendFile('index.html', {
+        root: path.resolve(PWD, 'frontend')
+    })
+
 });
 
 app.listen(5000, () => {console.log("Server started at http://localhost:5000")});
